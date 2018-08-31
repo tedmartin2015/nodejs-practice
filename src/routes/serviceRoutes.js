2aad009936dbea66c5cf0ad9cbf90cf38f9d054d
Ted Hermosisima <thermosisima@Ted-Legendary-MacbookPRO.local>
2018-08-31 09:30:41 +0800

    Update master with .add middleware

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -19,24 +19,28 @@ function router(message) {
       } ());
     });
 
-  serviceRouter.route('/:id')
-    .get((req, res) => {
-      (async function query() {
-        const { id } = req.params;
-        const request = new sql.Request();
-        const { recordset } = await request
-          .input('xServiceID', sql.Int, id)
-          .execute('dbo.pSELECTServicesByID');
+    serviceRouter.route('/:id')
+      //test commit to new branch
+      .all((req, res, next) => {
+        (async function query() {
+          const { id } = req.params;
+          const request = new sql.Request();
+          const { recordset } = await request
+            .input('xServiceID', sql.Int, id)
+            .execute('dbo.pSELECTServicesByID');
+
+          [req.service] = recordset;
+          next();
+        } ());
+      })
+      .get((req, res) => {
         res.render(
           'serviceView',
           {
-            service: recordset[0]
+            service: req.service
           }
         );
-      } ());
-
-
-    });
+      });
 
   return serviceRouter;
 }

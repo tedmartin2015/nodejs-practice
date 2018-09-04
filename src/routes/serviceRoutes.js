8a80c8226bfdb1cbdf1d7661e793d8baae632ba8
Ted Martin <ted.martin.hermosisima@gmail.com>
2018-09-04 10:16:55 +0800

    updated the serviceRoutes.js to use mongo client

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const sql = require('mssql');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const serviceRouter = express.Router();
 const debug = require('debug')('app:serviceRoutes');
 
@@ -27,32 +27,38 @@ function router(message) {
               client.close();
           }
           catch (err) {
+
             debug(err.stack);
           }       
       } ());
     });
 
   serviceRouter.route('/:id')
-    //test commit to new branch
-    .all((req, res, next) => {
-      (async function query() {
-        const { id } = req.params;
-        const request = new sql.Request();
-        const { recordset } = await request
-          .input('xServiceID', sql.Int, id)
-          .execute('dbo.pSELECTServicesByID');
-
-        [req.service] = recordset;
-        next();
-      } ());
-    })
+    /////test commit to new branch
     .get((req, res) => {
-      res.render(
-        'serviceView',
-        {
-          service: req.service
-        }
-      );
+      const { id } = req.params;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'serviceApp';
+
+      (async function mongo() {
+        let client;
+          try {
+              client = await MongoClient.connect(url);
+              const db = client.db(dbName);
+              const col = await db.collection('services');
+
+              const service = await col.findOne({ _id: new ObjectID(id)});
+              res.render(
+                'serviceView',
+                {
+                  service
+                }
+              );
+          }
+          catch (err) {
+            debug(err.stack);
+          }
+      } ());     
     });
 
   return serviceRouter;
